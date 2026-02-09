589c42a2f26a14ed5c996264519b0c47ac09fce6
JalenHu <jalenhu@byu.edu>
2026-02-09 07:41:31 -0700

    Add calculator page and update layout

---
+++ Mission5/wwwroot/js/calculator.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+
+    $("#calculateBtn").click(function () {
+
+        $("#errorMsg").hide();
+
+        let hours = parseFloat($("#hours").val());
+        let rate = parseFloat($("#rate").val());
+
+        if (isNaN(hours) || hours <= 0) {
+            $("#total").val("");
+            $("#errorMsg").show();
+            return;
+        }
+
+        let total = hours * rate;
+        $("#total").val("$" + total.toFixed(2));
+    });
+
+});
+
+
