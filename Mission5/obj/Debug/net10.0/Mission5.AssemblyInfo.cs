f4ec51d7694e523b716a064a41567ff0ff57daa3
JalenHu <jalenhu@byu.edu>
2026-02-10 18:25:11 -0700

    Fix the js file

--- Mission5/obj/Debug/net10.0/Mission5.AssemblyInfo.cs
+++ Mission5/obj/Debug/net10.0/Mission5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+589c42a2f26a14ed5c996264519b0c47ac09fce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
